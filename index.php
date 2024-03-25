0f4461c571e7ab07d9cb93b04a85ed7c43ead53a
ISABELLE NASCIMENTO DE OLIVEIRA <nascimento.isabelle@alunoescolassociais.org.br>
2024-03-25 08:33:40 -0300

    codigos

---
+++ index.php
@@ -0,0 +1,24 @@
+
+
+<?php
+if ((isset($_GET['nome']))) {
+    $nome = $_GET['nome'];
+}
+
+if ((isset($_GET['M_Semanal']))) {
+    $M_Semanal= $_GET['nome'];
+
+
+}
+if ((isset($_GET['M_Mensal']))) {
+    $m_Mensal = $_GET['M_Mensal'];
+
+    $M_Semanal +
+
+
+}
+
+?>
+
+
+   
